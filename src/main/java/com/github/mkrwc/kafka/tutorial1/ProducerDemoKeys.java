a6858db35ca94cf835c6f004a956b71b55831662
mkrwc <m.karwacki@protonmail.ch>
2021-11-04 20:57:32 +0100

    added multuple producer demo

---
+++ src/main/java/com/github/mkrwc/kafka/tutorial1/ProducerDemoKeys.java
@@ -0,0 +1,64 @@
+package com.github.mkrwc.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerDemoKeys {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // creating producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // creating the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i=0; i<10; i++) {
+            //create producer record
+
+            String topic = "first_topic";
+            String value = "hello world";
+            String key = "id_" + Integer.toString(i);
+            ProducerRecord<String, String> record =
+                    new ProducerRecord<String, String>(topic, key, value);
+
+            logger.info("Key: " + key);
+
+            // send data
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    // executes every time a record is successfully sent or an exception is thrown
+                    if (e == null) {
+                        // the record was successfully sent
+                        logger.info("Received new metadata. \n" +
+                                "Topic:" + recordMetadata.topic() + "\n" +
+                                "Partition:" + recordMetadata.partition() + "\n" +
+                                "Offset:" + recordMetadata.offset() + "\n" +
+                                "Timestamp:" + recordMetadata.timestamp());
+                    } else {
+                        logger.error("Error while producing", e);
+                    }
+                }
+            }).get(); // block .send() to make it synchronous -- bad practice
+        }
+
+        // create a producer record
+
+        // flush data
+        producer.flush();
+        // flush and close
+        producer.close();
+    }
+}
